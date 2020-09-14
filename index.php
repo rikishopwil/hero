598d94ad5bb38aefcbde01ce4a5e4f80b3db560d
rikishopwil <71271750+rikishopwil@users.noreply.github.com>
2020-09-14 19:19:57 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+// Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&utm_placement={{placement}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}}&fbpixel={{fbpixel}} 
+
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClient('http://kresto-trata.life/api.php?', 'Cq6bvXP5Y8YbM9xv');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
 <!DOCTYPE html>
 <html class="no-js mh-one-sb" lang="bg-BG">
 <head>
